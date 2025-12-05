2bd625624aa54124c656b59c439fdb0b59a044a7
notcelsius <149901647+notcelsius@users.noreply.github.com>
2025-12-05 01:15:23 -0800

    Time: 0 ms (100.00%) | Memory: 22.3 MB (54.97%) - LeetSync

--- 875-longest-mountain-in-array/longest-mountain-in-array.cpp
+++ 875-longest-mountain-in-array/longest-mountain-in-array.cpp
@@ -3,25 +3,29 @@ public:
     int longestMountain(vector<int>& arr) {
         if (arr.size() < 3) {
             return 0;
-        }   
+        }
         int maxLength = 0;
-        for (int i = 1; i < arr.size() - 1; i++) {
-            int l = i;
-            int r = i;
-            bool decrease = false;
-            bool increase = false;
-            while (l >= 1 && arr[l] > arr[l-1]) {
-                l--;
-                decrease = true;
+        int i = 0;
+        while (i < arr.size() - 1) {
+            int l = 0;
+            int r = 0;
+            while (i < arr.size() - 1 && arr[i] < arr[i+1]) {
+                l++;
+                i++;
             }
-            while (r < arr.size() - 1 && arr[r] > arr[r+1]) {
+            if (l == 0) {
+                i++;
+                continue;
+            }
+            while (i < arr.size() - 1 && arr[i] > arr[i+1]) {
                 r++;
-                increase = true;
+                i++;
             }
-            if (increase && decrease) {
-                maxLength = max(maxLength, r - l + 1);
+            if (r == 0) {
+                i++;
+                continue;
             }
-           
+            maxLength = max(maxLength, r + l + 1);
         }
         return maxLength;
     }

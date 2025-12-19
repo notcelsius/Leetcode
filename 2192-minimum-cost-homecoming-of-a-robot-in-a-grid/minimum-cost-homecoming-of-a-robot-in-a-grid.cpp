704e2993f75286cea6334ab93bae13f978a5d3cc
notcelsius <149901647+notcelsius@users.noreply.github.com>
2025-12-19 12:50:54 -0800

    Time: 1399 ms (5.11%) | Memory: 153.7 MB (83.40%) - LeetSync

---
+++ 2192-minimum-cost-homecoming-of-a-robot-in-a-grid/minimum-cost-homecoming-of-a-robot-in-a-grid.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minCost(vector<int>& startPos, vector<int>& homePos, vector<int>& rowCosts, vector<int>& colCosts) {
+        int cost = 0;
+        // while not on the same row
+        int i = startPos[0];
+        int j = homePos[0];
+        while (i != j) {
+            if (i < j) {
+                i++;
+                cost += rowCosts[i];
+            }
+            else {
+                i--;
+                cost += rowCosts[i];
+            }
+            cout << cost << endl;
+        }
+        // while not on same col
+        i = startPos[1];
+        j = homePos[1];
+        while (i != j) {
+            if (i < j) {
+                i++;
+                cost += colCosts[i];
+            }
+            else {
+                i--;
+                cost += colCosts[i];
+            }
+            cout << cost << endl;
+        }
+
+        return cost;
+    }
+};
